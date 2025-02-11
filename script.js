4310eddfdfeb719d4ea767c3b01b371b86ecec25
VSrajawat0022 <vishalsinghrajawat0022@gmail.com>
2025-02-11 11:16:39 +0530

    Push

---
+++ script.js
@@ -0,0 +1,18 @@
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('nav ul').classList.toggle('active');
+});
+
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        
+        window.scrollTo({
+            top: targetSection.offsetTop - 60,
+            behavior: 'smooth'
+        });
+        
+        document.querySelector('nav ul').classList.remove('active');
+    });
+});
